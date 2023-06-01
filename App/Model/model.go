dff3bf8a8d9dca37cd3833310b11c5a4ca2a0a10
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-06-01 17:32:18 +0000

    Model:Done

--- App/Model/model.go
+++ App/Model/model.go
@@ -1,1 +1,13 @@
 package model
+
+type GoShort struct {
+	ID       uint64 `json:"id" gorm:"primaryKey"` //Go Object Relational Mapping,
+	Redirect string `json:"redirect" gorm:"not null"`
+	GoShort  string `json:"goshort" gorm:"unique;not null"`
+	Clicked  uint64 `json:"clicked"`
+	Random   bool   `json:"random"`
+}
+
+func Setup() {
+
+}
