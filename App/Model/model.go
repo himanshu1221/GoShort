061efa4c3da3a4470581c85ac9f9f76b030b7a8d
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-06-02 16:32:44 +0530

    setup to connect postgres sql

--- App/Model/model.go
+++ App/Model/model.go
@@ -1,5 +1,12 @@
 package model
 
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
 type GoShort struct {
 	ID       uint64 `json:"id" gorm:"primaryKey"` //Go Object Relational Mapping,
 	Redirect string `json:"redirect" gorm:"not null"`
@@ -9,5 +16,14 @@ type GoShort struct {
 }
 
 func Setup() {
-
+	//IP Address of postgres sql running using docker
+	//DSN represent data source name
+	//It is used to establish a conection between postgres database
+	//SSL (Secure Sockets Layer) is a cryptographic protocol used to secure communications over a network, such as the internet. In the context of the sslmode parameter in the PostgreSQL connection string, it determines how the Go application will handle SSL/TLS encryption when connecting to the PostgreSQL database.
+	dsn := "host=172.17.0.2 user=admin password=test dbname=admin port=5432 sslmode=disable"
+	var err error
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
 }
