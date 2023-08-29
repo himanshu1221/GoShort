25ceadbaff851861dfdab7ffedb153e0512bc0d7
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-08-29 16:03:53 +0530

    added DSN:Data source name

--- App/Model/model.go
+++ App/Model/model.go
@@ -7,3 +7,7 @@ type Goshort struct {
 	Clicked  uint64 `json:"clicked"`
 	Random   bool   `json:"random"`
 }
+
+func Setup() {
+	dsn := "host=172.17.0.2 user=admin password=test dbname=admin port=5432 sslmode=disable"
+}
