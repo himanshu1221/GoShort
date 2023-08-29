824a340d2c79f0b366842b6eb564c4c04a346139
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-08-29 08:23:45 +0000

    created structs

--- App/Model/model.go
+++ App/Model/model.go
@@ -1,1 +1,9 @@
 package model
+
+type Goshort struct {
+	ID       uint64 `json:"id" gorm:"primaryKey"`
+	Redirect string `json:"redirect"`
+	Goshort  string `json:"goshort" gorm:"unique;not null"`
+	Clicked  uint64 `json:"clicked"`
+	Random   bool   `json:"random"`
+}
