aba80131397cf11ca644cd0d4a3eb0884c1b5e53
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-09-01 13:05:17 +0530

    connection established

--- App/Model/model.go
+++ App/Model/model.go
@@ -1,5 +1,12 @@
 package model
 
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
 type Goshort struct {
 	ID       uint64 `json:"id" gorm:"primaryKey"`
 	Redirect string `json:"redirect"`
@@ -10,4 +17,9 @@ type Goshort struct {
 
 func Setup() {
 	dsn := "host=172.17.0.2 user=admin password=test dbname=admin port=5432 sslmode=disable"
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
 }
