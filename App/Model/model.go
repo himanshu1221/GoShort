552a902f9b784135f72b2c08c412dadb77bf6223
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-09-02 16:57:13 +0530

    added learnings

--- App/Model/model.go
+++ App/Model/model.go
@@ -16,8 +16,17 @@ type Goshort struct {
 }
 
 func Setup() {
+
+	// DSN standds for data source name which consisit information like host,user,pass,dbname port sslmode
+
 	dsn := "host=172.17.0.2 user=admin password=test dbname=admin port=5432 sslmode=disable"
+
 	var err error
+
+	// gorm.Open is a function provided by GORM for creating a database connection.
+	// &gorm.Config{} is used to configure the behavior of the GORM instance. In this case, it uses the default configuration.
+	// postgres.Open(dsn) specifies that the PostgreSQL driver should be used, and it takes the DSN string dsn as an argument to establish the connection.
+
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
