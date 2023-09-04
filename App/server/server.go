6fc2285088c33811db437dd115149fe86dbe8945
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-09-04 15:02:32 +0530

    get single rediret

--- App/server/server.go
+++ App/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	model "github.com/himanshu1221/GoShort/Model"
@@ -16,6 +18,22 @@ func GetAllRedirects(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusOK).JSON(shorts)
 }
 
+func GetRedirect(c *fiber.Ctx) error {
+	id, err := strconv.ParseUint(c.Params("id"), 10, 64)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "error could not parse id" + err.Error(),
+		})
+	}
+	short, err := model.SingleShorts(id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "error could not short from DB" + err.Error(),
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(short)
+}
+
 func SetupAndListen() {
 	router := fiber.New()
 
@@ -24,5 +42,6 @@ func SetupAndListen() {
 		AllowHeaders: "Origin,Content-Type,Accept",
 	}))
 	router.Get("/shorts", GetAllRedirects)
+	router.Get("/shorts/:id", GetRedirect)
 	router.Listen(":3000")
 }
